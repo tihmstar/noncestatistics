d4f444b1ace0b8fc640115f43f0665b5087bf7af
Malte Poll <malte.poll@web.de>
2016-09-09 19:51:07 +0200

    Added Makefile for STATIC compilation

--- noncestatistics/compile_brew.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-clang *.c *.h *.cpp /usr/local/lib/libplist.a /usr/local/lib/libxml2.a /usr/local/lib/liblzma.a -liconv /usr/local/lib/libimobiledevice.a /usr/local/lib/libusbmuxd.a /usr/local/lib/libirecovery.a -lz -framework IOKit -lc++ -framework CoreFoundation -lssl /usr/local/Cellar/openssl/1.0.2h_1/lib/libcrypto.a 
